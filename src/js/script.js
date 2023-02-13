1e89b10581532434c4ebf81018ad260aa46959c5
Sujit <sb2011ams@gmail.com>
2023-02-13 11:07:35 +0100

    first draft

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const messages = [
+  '"The purpose of our lives is to be happy." — Dalai Lama',
+  '"If you want to live a happy life, tie it to a goal, not to people or things." – Albert Einstein',
+  "“The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.” – Steve Jobs",
+  "“If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.” – Steve Jobs",
+  "“If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.” – Steve Jobs",
+];
+
+const updateMessage = (i) => {
+  const message = document.getElementById("message");
+  message.innerHTML = messages[i];
+};
+
+document.querySelector("#message").innerHTML +=
+  messages[Math.floor(Math.random() * messages.length)];
+
+document.getElementById("nextMessage").addEventListener("click", function () {
+  updateMessage(Math.floor(Math.random() * messages.length));
+});
